6d8dd888af8435b768c9a9fc7ec9bc5187b9db0e
michal-karas <60525701+michal-karas@users.noreply.github.com>
2020-04-10 13:12:31 +0200

    creating a template
    creating a template

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let navigation = document.getElementById('navigation');
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 0) {
+        navigation.classList.add('active');
+    } else {
+        navigation.classList.remove('active');
+    }
+})
+
+let hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+})
